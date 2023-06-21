00bf815404d6b7deedd94ec150a05977347a98be
pekoisgood <peko.claire@gmail.com>
2023-06-21 17:09:25 +0800

    Feature: chat room defferentiate message sender.

--- src/app/bookClub/Chatroom.tsx
+++ src/app/bookClub/Chatroom.tsx
@@ -18,6 +18,7 @@ type Message = {
   room: string;
   createdAt: Timestamp;
   id: string;
+  userId: string;
 };
 
 const Chatroom = ({
@@ -32,6 +33,7 @@ const Chatroom = ({
   const chatRoomMessagesRef = useRef<HTMLDivElement | null>(null);
 
   const roomId = "testRoom";
+  const userId = "peko123";
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -41,6 +43,7 @@ const Chatroom = ({
       text: newMessage,
       createdAt: serverTimestamp(),
       user: "Peko",
+      userId: "peko123",
       room: roomId,
     });
 
@@ -54,13 +57,7 @@ const Chatroom = ({
       orderBy("createdAt")
     );
     const unsuscribe = onSnapshot(queryMessages, (snapshot) => {
-      let messages: {
-        user: string;
-        text: string;
-        room: string;
-        createdAt: Timestamp;
-        id: string;
-      }[] = [];
+      let messages: Message[] = [];
       snapshot.forEach((doc) => {
         messages.push({
           user: doc.data().user,
@@ -68,6 +65,7 @@ const Chatroom = ({
           text: doc.data().text,
           createdAt: doc.data().createdAt,
           id: doc.id,
+          userId: doc.data().userId,
         });
       });
 
@@ -93,8 +91,13 @@ const Chatroom = ({
           className="flex flex-col gap-[5px] h-[full] overflow-scroll overscroll-y-contain p-2 grow"
         >
           {messages.map((message) => (
-            <div key={message.id}>
-              <span>{message.user} : </span>
+            <div
+              key={message.id}
+              className={`flex ${
+                message.userId === userId ? "justify-end" : "justify-start"
+              }`}
+            >
+              {message.userId !== userId && <span>{message.user} : </span>}
               {message.text}
             </div>
           ))}
