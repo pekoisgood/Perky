f05bdcd63348be39f953c565f9440fb234e3f51b
pekoisgood <peko.claire@gmail.com>
2023-06-22 15:13:02 +0800

    Fix: eslint error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-export default function Home() {
-<<<<<<< HEAD
-  return <>Home</>;
-=======
-  return <div>Home</div>;
-=======
-import Image from "next/image";
-
 export default function Home() {
   return <>Home</>;
->>>>>>> Feature: build feature of book club video, chat room ,and note in bookClub page.
->>>>>>> Feature: build feature of book club video, chat room ,and note in bookClub page.
 }
