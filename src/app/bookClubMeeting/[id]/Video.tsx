e5be7700c4f7df7c0fdc4d72833de168a02289b5
pekoisgood <peko.claire@gmail.com>
2023-07-05 13:56:23 +0800

    Fix: Api key in video SDK.

--- src/app/bookClubMeeting/[id]/Video.tsx
+++ src/app/bookClubMeeting/[id]/Video.tsx
@@ -19,7 +19,8 @@ const MeetingView = dynamic(
 );
 
 const Video = ({ roomId }: Props) => {
-  const API_KEY = process.env.NEXT_PUBLIC_VIDEO_SDK_API_KEY;
+  const API_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI0YzcyNzQ0Yi03M2RkLTQ2MzctYTFkMS0xZGUxODY2NGFmNTQiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTY4NzA2ODQ0MiwiZXhwIjoxNjk0ODQ0NDQyfQ.RHWaGaFGbMDATcOixhDZmswjA7FSzb7-b7pOjg9iG7g";
 
   if (!roomId || !API_KEY) return <></>;
 
