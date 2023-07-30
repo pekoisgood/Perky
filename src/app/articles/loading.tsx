0c5443c7363f44318204769c88efa1e6cdd06cbe
pekoisgood <peko.claire@gmail.com>
2023-07-30 17:40:34 +0800

    Fix: add margin top to articles page skeleton h2 title.

--- src/app/articles/loading.tsx
+++ src/app/articles/loading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const loading = () => {
   return (
     <div className="flex flex-col gap-3 w-full">
-      <h1 className="mx-auto w-[200px] rounded-lg max-w-full h-[35px] bg-gradient-to-r from-[#eee] via-[#d6d6d6] to-[#eee] bg-[length:400%_400%] animate-skeleton" />
+      <h1 className="mt-[40px] mx-auto w-[200px] rounded-lg max-w-full h-[35px] bg-gradient-to-r from-[#eee] via-[#d6d6d6] to-[#eee] bg-[length:400%_400%] animate-skeleton" />
       <div className="flex flex-wrap w-full">
         <ArticleListSkeleton />
         <ArticleListSkeleton />
