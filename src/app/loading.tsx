71e31f41cde2b04acd2ab4fdb332fe158f1db868
pekoisgood <peko.claire@gmail.com>
2023-07-31 15:23:36 +0800

    Fix: add loading file in the root app folder.

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      loading
+    </div>
+  );
+};
+
+export default loading;
