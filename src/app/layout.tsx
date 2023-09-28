0da704d21f22ed33c054b656aba1a706656109a1
Peko Chuang <pekochuang@Pekos-MacBook-Air.local>
2023-09-28 18:46:22 +0800

    Fix: add GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 
 import "./globals.css";
 
@@ -25,6 +26,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-DWFHWZTW7X"
+        ></Script>
+        <Script id="gtag-init" strategy="lazyOnload">
+          {` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', ${process.env.NEXT_PUBLIC_GA_KEY});`}
+        </Script>
+      </head>
       <body
         className={`box-border ${poppins.className} transform-none relative mx-auto`}
       >
