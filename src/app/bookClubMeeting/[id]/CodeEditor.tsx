2a24beaa72c6a8d685b83a59307e20f11ed9624c
pekoisgood <peko.claire@gmail.com>
2024-11-13 11:39:59 +0800

    fix: comment relative function and feature with rapid api

--- src/app/bookClubMeeting/[id]/CodeEditor.tsx
+++ src/app/bookClubMeeting/[id]/CodeEditor.tsx
@@ -50,69 +50,69 @@ export const getExtensionLanguage = (lang: string) => {
 
 // TODO: code compiler not working, disable for temp
 export default function CodeEditor({ code, setCode }: Props) {
-  const [output, setOutput] = useState<string>("");
-  const [isRunning, setIsRunning] = useState(false);
+  // const [output, setOutput] = useState<string>("");
+  // const [isRunning, setIsRunning] = useState(false);
   const [language, setLanguage] = useState<string>("Javascript");
 
-  const handleRunCode = () => {
-    setIsRunning(true);
-    compile();
-  };
+  // const handleRunCode = () => {
+  //   setIsRunning(true);
+  //   // compile();
+  // };
 
-  const compile = async () => {
-    const url = "https://judge0-ce.p.rapidapi.com/submissions";
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-RapidAPI-Key": rapidApiKey ?? "",
-        "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
-      },
-      body: JSON.stringify({
-        language_id: languageIds[language as keyof typeof languageIds],
-        source_code: code,
-        stdin: "SnVkZ2Uw",
-      }),
-    };
+  // const compile = async () => {
+  //   const url = "https://judge0-ce.p.rapidapi.com/submissions";
+  //   const options = {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "X-RapidAPI-Key": rapidApiKey ?? "",
+  //       "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
+  //     },
+  //     body: JSON.stringify({
+  //       language_id: languageIds[language as keyof typeof languageIds],
+  //       source_code: code,
+  //       stdin: "SnVkZ2Uw",
+  //     }),
+  //   };
 
-    try {
-      const response = await fetch(url, options);
-      const result = await response.text();
-      const token = result.split(":")[1].split("}")[0].split('"')[1];
-      checkStatus(token);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  //   try {
+  //     const response = await fetch(url, options);
+  //     const result = await response.text();
+  //     const token = result.split(":")[1].split("}")[0].split('"')[1];
+  //     checkStatus(token);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
-  const checkStatus = async (token: string) => {
-    const url = `https://judge0-ce.p.rapidapi.com/submissions/${token}?base64_encoded=true&fields=*`;
+  // const checkStatus = async (token: string) => {
+  //   const url = `https://judge0-ce.p.rapidapi.com/submissions/${token}?base64_encoded=true&fields=*`;
 
-    const headers = {
-      "X-RapidAPI-Key": rapidApiKey ?? "",
-      "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
-    };
-    try {
-      const response = await fetch(url, { method: "GET", headers });
-      const data = await response.json();
-      if (!data.status) return;
-      const statusId: number = data.status.id;
+  //   const headers = {
+  //     "X-RapidAPI-Key": rapidApiKey ?? "",
+  //     "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
+  //   };
+  //   try {
+  //     const response = await fetch(url, { method: "GET", headers });
+  //     const data = await response.json();
+  //     if (!data.status) return;
+  //     const statusId: number = data.status.id;
 
-      if (statusId === 1 || statusId === 2) {
-        setTimeout(() => {
-          checkStatus(token);
-        }, 2000);
-        return;
-      } else {
-        setOutput(atob(data.stdout));
-        setIsRunning(false);
-        return;
-      }
-    } catch (err) {
-      setIsRunning(false);
-      setOutput("error");
-    }
-  };
+  //     if (statusId === 1 || statusId === 2) {
+  //       setTimeout(() => {
+  //         checkStatus(token);
+  //       }, 2000);
+  //       return;
+  //     } else {
+  //       setOutput(atob(data.stdout));
+  //       setIsRunning(false);
+  //       return;
+  //     }
+  //   } catch (err) {
+  //     setIsRunning(false);
+  //     setOutput("error");
+  //   }
+  // };
 
   return (
     <div className="m-1 h-full rounded-xl">
@@ -129,7 +129,7 @@ export default function CodeEditor({ code, setCode }: Props) {
             );
           })}
         </select>
-        <button
+        {/* <button
           onClick={handleRunCode}
           className={`h-[27px] rounded-lg bg-orange-100 px-[5px] ${
             isRunning && "text-[12px]"
@@ -137,7 +137,7 @@ export default function CodeEditor({ code, setCode }: Props) {
         `}
         >
           {isRunning ? "running" : <VscDebugStart />}
-        </button>
+        </button> */}
       </div>
       <div className="h-[90%] overflow-y-scroll">
         <div className="h-[84%] overflow-hidden rounded-t-lg">
@@ -154,10 +154,10 @@ export default function CodeEditor({ code, setCode }: Props) {
           />
         </div>
 
-        <div className="mb-2 h-[16%] overflow-y-scroll rounded-b-lg border-t-[1px] border-dashed border-white bg-black p-2 text-white">
+        {/* <div className="mb-2 h-[16%] overflow-y-scroll rounded-b-lg border-t-[1px] border-dashed border-white bg-black p-2 text-white">
           <h2 className="m-0">output :</h2>
           <div className="w-full overflow-y-scroll">{output}</div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
